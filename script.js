cc26af50f4599a9ec42eaf1d8eb24bbe6966c486
Vaibhavdike93 <vaibhav.dike93@gmail.com>
2025-01-08 14:12:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const navbar = document.getElementById('navbar');
+
+// Change background color of navbar when scrolling
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
